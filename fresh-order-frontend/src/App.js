75a04c1466329289856e25b936ed8c077c2297c5
sohail butt <sohailbutt@sohails-MacBook-Air.local>
2025-06-19 14:31:06 -0400

    Add React frontend

---
+++ fresh-order-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import OrdersList from './OrdersList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fresh Order API Frontend</h1>
+      <OrdersList />
+    </div>
+  );
+}
+
+export default App;
